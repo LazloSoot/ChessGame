f07d6bd6b4f8c95014ebd8809600f12d7db7bb00
lazlosoot1911 <free2play1911@gmail.com>
2018-12-12 18:09:39 +0200

    angular material installed

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { MaterialModule } from './modules/material/material.module';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
+    MaterialModule,
     CoreModule,
     SharedModule,
 
