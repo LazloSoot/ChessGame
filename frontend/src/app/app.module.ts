ca551756a85c4d3d496b24a22f210d76f4abc30c
lazlosoot1911 <free2play1911@gmail.com>
2018-12-12 17:42:48 +0200

    frontend initial

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    CoreModule,
+    SharedModule,
+
+    AppRoutingModule // must be imported as the last module as it contains the fallback route
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ frontend/src/app/shared/shared.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './layout/header.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [HeaderComponent]
+})
+export class SharedModule { }
